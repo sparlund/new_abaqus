4956131ef4a2e96ddc87b53f8fdf043da43fa3db
sparlund <t>
2021-01-01 23:11:38 +0100

    fiddling

--- src/new_abaqus.h
+++ src/new_abaqus.h
@@ -1,13 +1,13 @@
 #pragma once
+#include <string>
 #include "mesh.h"
-
-// this class should hold a mesh and results
 class New_abaqus
 {
 private:
 public:
     Mesh mesh;
-    New_abaqus(/* args */);
+    New_abaqus();
+
     ~New_abaqus();
 };
 
