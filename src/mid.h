3a7c911f9d4d4f1bb13e415d63315b97cdd66a28
sparlund <t>
2021-01-24 14:19:26 +0100

    trying to make materials a little more smarter

--- src/mid.h
+++ src/mid.h
@@ -2,14 +2,20 @@
 #include <string>
 #include <vector>
 #include <unordered_map>
-		
+#include <Eigen/Dense>
+
 class Mid
 {
 private:
+    // If the input file contains anything that indicates a non linear material model, set this boolean false
+    bool linear=true;
     float density;
     float E;
     float v;
     std::string name;
+    // Constitutive matrix (linear continuum mechanics)
+    Eigen::Matrix<float,6,6> D_3D_linear_continuum_mechanics;
+    Eigen::Matrix<float,3,3> D_2D_linear_continuum_mechanics;
 public:
     void set_density(float density){this->density = density;};
     void set_E(float E){this->E = E; };
@@ -19,6 +25,7 @@ public:
     float get_density(){return density;};
     float get_E(){return E;};
     float get_v(){return v;};
+    // fill this vector with the supported material keywords in mid.cpp
     static const std::vector<std::string> supported_material_keywords;
     Mid(std::string name);
     ~Mid();
