58047683179e84400cf35b2fa7f3c7f34cd2e555
sparlund <t>
2021-08-24 16:54:01 +0200

    setting up clang-tidy!

---
+++ tests/run_clang_tidy.py
@@ -0,0 +1,13 @@
+import os
+import glob
+import subprocess
+
+if len(glob.glob("compile_commands.json")) == 0:
+    print("compile_commands.json doesn't exists")
+
+files = glob.glob("../src/*.cpp")
+
+for counter,file in enumerate(files):
+    print("File #"+str(counter)+"/"+str(len(files))+": "+file)
+    os.system("clang-tidy -checks=\"*\" "+file+" > " + file.split("/")[-1].split(".")[0]+"_clang_tidy.txt 2> /dev/null")
+
