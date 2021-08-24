2c6e8cd1751e460724e0457dff580f638258d2d9
sparlund <t>
2021-08-24 17:30:54 +0200

    clang-tidy warnings fix on pid

--- src/pid.cpp
+++ src/pid.cpp
@@ -1,10 +1,11 @@
-#include <iostream>
 #include "pid.h"
 #include "mid.h"
+#include <iostream>
 
+std::shared_ptr<Mid> Pid::get_mid(){return this->mid;};
+
+std::unordered_map<std::string,std::string>* Pid::get_options(){return &options;};
 
 Pid::Pid(std::string name,std::shared_ptr<Mid> mid):name(name),mid(mid){
     std::cout << "*SECTION: ELSET = " << this->name << ", MATERIAL = " << this->mid->get_name() << std::endl;
 }
-
-Pid::~Pid(){}
