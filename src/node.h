7a839dbf0677e090f3eddc03067e3ea28f0194a2
sparlund <t>
2021-10-05 22:29:14 +0200

    changing node counting logic, nodes own Dofs

--- src/node.h
+++ src/node.h
@@ -1,16 +1,14 @@
 #pragma once
 #include "dof.h"
 #include <array>
+#include <memory>
 #include <vector>
 
 class Node
 {
-private:
-    static unsigned int node_counter;
 public:
     const unsigned int id;
     const float x, y, z;
-    std::vector<Dof> dofs;  
-    unsigned int get_node_counter();
+    std::vector<std::unique_ptr<Dof>> dofs;
     Node(unsigned int global_id, float x, float y, float z = 0);
 };
