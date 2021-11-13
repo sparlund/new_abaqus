a2bc5ccc2f1583a109d616a17d8c57d90f3526f9
sparlund <t>
2021-11-13 21:49:54 +0100

    changing file structure lol

--- src/mid.cpp
+++
@@ -1,9 +0,0 @@
-#include "mid.h"
-#include <iostream>
-
-const std::vector<std::string> Mid::supported_material_keywords = { "*ELASTIC",
-                                                                    "*DENSITY"};
-Mid::Mid(const std::string& name):linear{true},density{0.0f},E{0.0f},v{0.0f},name{name}
-{
-    std::cout << "*MATERIAL: name=" << name << std::endl;
-};
--- src/node.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "node.h"
-#include "dof.h"
-
-Node::Node(unsigned int id, float x, float y, float z):id{id},x{x},y{y},z{z}
-{
-    // dofs will be added later to the node when we know what type of element will use it!     
-    std::cout << "*NODE: id=" << id << ", x=" << x << ", y=" << y << ", z=" << z << std::endl;   
-};
