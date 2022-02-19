27fe5be417bfc7555c8f3df93bbf3bc15752683a
sparlund <t>
2022-02-19 13:28:36 +0100

    change source folder

--- tests/run_clang_tidy.py
+++ tests/run_clang_tidy.py
@@ -5,7 +5,7 @@ import subprocess
 if len(glob.glob("compile_commands.json")) == 0:
     print("compile_commands.json doesn't exists")
 
-files = glob.glob("../src/*.cpp")
+files = glob.glob("../src/src/*.cpp")
 
 for counter,file in enumerate(files):
     print("File #"+str(counter+1)+"/"+str(len(files))+": "+file + " --> " + file.split("/")[-1].split(".")[0] +"_clang_tidy.txt")
